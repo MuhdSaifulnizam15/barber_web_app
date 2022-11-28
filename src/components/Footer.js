fa76c35298515ede06fe789b06646f2a1aa7035d
Muhd Saiful <muhdsaifulnizam1998@gmail.com>
2022-11-28 22:21:17 +0800

    feat: added auth screen

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer class="footer footer-center  w-full p-4">
+      <div class="text-center">
+        <p>
+          Copyright © 2022 -{" "}
+          <a class="font-semibold" href="mailto:m.sohanemon@gmail.com">
+            Muhd Saiful
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
