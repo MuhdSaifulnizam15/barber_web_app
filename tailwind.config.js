959a69da99ad70a2611ef7486bf7a2af1d891bd7
Muhd Saiful <muhdsaifulnizam1998@gmail.com>
2023-01-11 01:55:53 +0800

    feat: update on file structure

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      sans: ["Roboto", "sans-serif"],
+      open: ["Open Sans", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
