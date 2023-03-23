5f74e244caf99a2993bec565f1ea1a4b4c0a362d
Muhd Saiful <muhdsaifulnizam1998@gmail.com>
2023-03-23 10:04:38 +0800

    fix: modal scrollable issue

--- src/pages/Admin/Branch/index.js
+++ src/pages/Admin/Branch/index.js
@@ -168,7 +168,7 @@ const Branch = () => {
             {showModal ? (
               <>
                 <div className='flex justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
-                  <div className='relative w-full mx-6 md:mx-0 md:w-4/5 lg:w-3/5 my-6 mx-auto max-w-6xl'>
+                  <div className='relative w-full mx-6 md:mx-0 md:w-4/5 lg:w-3/5 my-6 mx-auto max-w-6xl h-5/6 overflow-y-auto'>
                     <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
                       <div className='flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t'>
                         <h3 className='text-3xl font-semibold'>
